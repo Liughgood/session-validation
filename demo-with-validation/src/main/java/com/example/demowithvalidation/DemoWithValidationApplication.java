422ee28c1db9a7c1c383f42f26593c39b30ab309
Genhao Liu <genhao.liu@thoughtworks.com>
2022-10-30 17:26:11 +0800

    [GenhaoLiu] #N/A feat: init project

---
+++ demo-with-validation/src/main/java/com/example/demowithvalidation/DemoWithValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demowithvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWithValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWithValidationApplication.class, args);
+    }
+
+}
---
+++ demo-without-validation/src/main/java/com/example/demowithoutvalidation/DemoWithoutValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demowithoutvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWithoutValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWithoutValidationApplication.class, args);
+	}
+
+}
